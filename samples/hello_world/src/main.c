4903f27b2d0b8dbfac05ea360c216eb96900304e
Fabian Blatz <fabianblatz@gmail.com>
2024-11-08 09:11:41 +0100

    make sample work

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,18 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+const struct device* sensor = DEVICE_DT_GET(DT_NODELABEL(test_i2c_vl53l5cx));
+
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+    sensor_sample_fetch_chan(sensor, SENSOR_CHAN_DISTANCE);
 
 	return 0;
 }
