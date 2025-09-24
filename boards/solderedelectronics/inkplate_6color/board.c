0622656e2190fb4cfdf6c1a2420b9e0a8ca75870
Fabian Blatz <fabianblatz@gmail.com>
2025-09-24 15:52:32 +0200

    boards: Add SolderedElectronics Inkplate 6Color board

---
+++ boards/solderedelectronics/inkplate_6color/board.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025 Fabian Blatz <fabianblatz@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
+
+#define EXPANDER_VBATT_GPIO_SPEC     GPIO_DT_SPEC_GET(DT_NODELABEL(vbatt), power_gpios)
+#define EXPANDER_SDCARD_EN_GPIO_SPEC GPIO_DT_SPEC_GET(DT_NODELABEL(sdhc_spi), pwr_gpios)
+
+static inline void configure_vbatt_measure(bool on)
+{
+	struct gpio_dt_spec vbatt_gpio = EXPANDER_VBATT_GPIO_SPEC;
+
+	if (!gpio_is_ready_dt(&vbatt_gpio)) {
+		return;
+	}
+
+	gpio_pin_configure_dt(&vbatt_gpio, (on ? GPIO_OUTPUT_ACTIVE : GPIO_OUTPUT_INACTIVE));
+}
+
+static inline void configure_sdcard_power(bool on)
+{
+	struct gpio_dt_spec sdcard_en_gpio = EXPANDER_SDCARD_EN_GPIO_SPEC;
+
+	if (!gpio_is_ready_dt(&sdcard_en_gpio)) {
+		return;
+	}
+
+	gpio_pin_configure_dt(&sdcard_en_gpio, (on ? GPIO_OUTPUT_ACTIVE : GPIO_OUTPUT_INACTIVE));
+}
+
+static int board_inkplate_6color_init(void)
+{
+
+	configure_vbatt_measure(false);
+	configure_sdcard_power(false);
+
+	return 0;
+}
+
+/* needs to be done after I2C driver init, which is at
+ * POST_KERNEL:CONFIG_GPIO_PCAL64XXA_INIT_PRIORITY.
+ * but before POST_KERNEL:CONFIG_SENSOR_INIT_PRIORITY for the voltage-divider
+ */
+SYS_INIT(board_inkplate_6color_init, POST_KERNEL, 80);
